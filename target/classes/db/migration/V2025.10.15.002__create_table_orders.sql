3159e7d6a0436ae5ca2e62afaf76d44684fba46d
isavrodrigues <isabelavr@al.insper.edu.br>
2025-10-24 18:17:24 -0300

    remove target directory from git tracking

--- target/classes/db/migration/V2025.10.15.002__create_table_orders.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE orders
-(
-    id varchar(36) NOT NULL,
-    id_user varchar(36) NOT NULL,
-    date timestamptz NOT NULL,
-    total decimal(12,2) NOT NULL,
-    items_json text,
-    CONSTRAINT orders_pkey PRIMARY KEY (id)
-);
